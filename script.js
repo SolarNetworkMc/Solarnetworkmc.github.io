76d67cdc9b04435a9004cc2fdd9076d47691703f
solarnetworkmc <114137384+solarnetworkmc@users.noreply.github.com>
2023-05-05 08:52:43 +0000

    fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let popup = document.getElementById('popup');
 let bar = document.getElementById('bar');
 let logo = document.getElementById('logobig');
-let iframe = document.querySelector('iframe');
+let help = document.querySelector('.help');
 let jobsitems = document.querySelector('.jobs');
 let back = document.querySelector('.back');
 let overlay = document.querySelector('.overlay');
@@ -9,20 +9,18 @@ let titlepopup = document.getElementById('titleofpopup');
 
 function openpopup() {
   popup.classList.toggle('active');
-  iframe.classList.toggle('active');
 overlay.classList.toggle('active');
 
 };
 
 function closepopup() {
   popup.classList.toggle('active');
-  iframe.classList.toggle('active');
 overlay.classList.toggle('active');
 
 };
 function helpapp() {
-  iframe.classList.toggle('toggle');
-  jobsitems.classList.toggle('toggle');
+  help.classList.toggle('toggle');
+jobsitems.classList.toggle('toggle');
 back.classList.toggle("toggle");
   if (titlepopup.innerText === "Staff Positions") {
  titlepopup.innerText = "Helper Application"
