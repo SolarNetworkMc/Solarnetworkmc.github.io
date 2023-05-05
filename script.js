ad4566ef7aee1c2a92cdaa42765cb4bf9fe2b924
solarnetworkmc <114137384+solarnetworkmc@users.noreply.github.com>
2023-05-05 09:12:13 +0000

    fix

--- script.js
+++ script.js
@@ -9,25 +9,25 @@ let titlepopup = document.getElementById('titleofpopup');
 
 function openpopup() {
   popup.classList.toggle('active');
-overlay.classList.toggle('active');
+  overlay.classList.toggle('active');
 
 };
 
 function closepopup() {
   popup.classList.toggle('active');
-overlay.classList.toggle('active');
+  overlay.classList.toggle('active');
 
 };
 function helpapp() {
   help.classList.toggle('toggle');
-jobsitems.classList.toggle('toggle');
-back.classList.toggle("toggle");
+  jobsitems.classList.toggle('toggle');
+  back.classList.toggle("toggle");
   if (titlepopup.innerText === "Staff Positions") {
- titlepopup.innerText = "Helper Application"
-} else {
+    titlepopup.innerText = "Helper Application"
+  } else {
     titlepopup.innerText = "Staff Positions"
+  }
 }
-} 
 
 /* window.addEventListener("scroll", function() {
   bar.classList.toggle("active", window.scrollY > 0);
