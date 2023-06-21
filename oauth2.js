eee16916b221e0c2123a9956bc05b8cf16291b1b
BlockG2500 <114137384+BlockG2500@users.noreply.github.com>
2023-06-21 08:43:32 +0000

    oops i destroyed everything luckily i restored

--- oauth2.js
+++ oauth2.js
@@ -2,21 +2,20 @@ window.onload = () => {
   const url = window.location.search;
   const urlParams = new URLSearchParams(url);
   const code = urlParams.get('code');
-
   if (code) {
     fetchUserProfile(code);
   } else {
-    alert(`No code provided`);
+    console.log(`nocode`);;
   }
 }
 
 function fetchUserProfile(code) {
   const requestBody = new URLSearchParams();
   requestBody.append('client_id', '1095020466852348024');
-  requestBody.append('client_secret', 'hidden_ajg3mpO6EnGaXY1_dm');
+  requestBody.append('client_secret', 'GthHZEULOXFAo_ajg3mpO6EnGaXY1_dm');
   requestBody.append('grant_type', 'authorization_code');
   requestBody.append('code', code);
-  requestBody.append('redirect_uri', 'https://blockg2500.github.io/testweb/');
+  requestBody.append('redirect_uri', 'https://solarnetworkmc.github.io/vacancies/helper');
   requestBody.append('scope', 'identify email guilds');
 
   fetch('https://discord.com/api/oauth2/token', {
@@ -35,29 +34,19 @@ function fetchUserProfile(code) {
     })
     .then(response => response.json())
     .then(user => {
-      console.log(user);
-      showUserInfo(user);
-      fetch('https://discord.com/api/v10/users/@me/guilds', {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`
-        }
-      })
-      .then(response => response.json())
-      .then(guilds => {
-        const targetGuildId = '1051145073389207592'; // Specific guild ID to check
-        const isMember = guilds.some(guild => guild.id === targetGuildId);
-      
-        if (isMember) {
-          alert(`you in solar omg`);
-          // Perform actions for users who are members of the server
-        } else {
-          alert(`bro join solar 😡😡😡`);
-          // Perform actions for users who are not members of the server
-        }
-      })
-      .catch(error => {
-        console.error('Failed to fetch guilds:', error);
-      });
+  document.getElementById('email').value = user.email;
+      if (user.discriminator === "0") {
+document.getElementById('discordname').value = `${user.username}`;
+      } else {
+document.getElementById('discordname').value = `${user.username}#${user.discriminator}`;
+      }
+    document.getElementById('ID').value = user.id;
+      if (user.id) {
+        gonelol();
+      } else {
+    document.getElementById('email').value = ""
+document.getElementById('discordname').value = ""
+      }
     })
     .catch(error => {
       console.error('Failed to fetch user data:', error);
@@ -67,7 +56,32 @@ function fetchUserProfile(code) {
     console.error('Token exchange failed:', error);
   });
 }
+function gonelol() { 
+let line = document.querySelector('.line');
+line.style.animationName = 'gone';
+line.style.animationDuration = '0.6s';
+line.style.animationFillMode = 'forwards';
+let lock = document.querySelector('.lockk');
+lock.style.animationName = 'fade';
+lock.style.animationDuration = '0.6s';
+lock.style.animationFillMode = 'forwards';
+setTimeout(() => {
+    deleteElementAndChildren('lock1');
+    deleteElementAndChildren('lock2'); 
+}, 700);
+}
+if (document.getElementById('email').value !== "")
+{
+document.querySelector('.if').style.display = "block";
+}
 
-function showUserInfo(user) {
-  alert(`ID: ${user.id}, Email: ${user.email}`);
+function deleteElementAndChildren(elementId) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    while (element.firstChild) {
+      deleteElementAndChildren(element.firstChild.id);
+      element.removeChild(element.firstChild);
+    }
+element.parentNode.removeChild(element);
+  }
 }
